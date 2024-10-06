ad271ff8a3de7ad0ea4d97fc3937f66f78bfd919
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-10-06 23:10:14 +0530

    Componenets Added to root layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import Footer from "@/components/Footer"
+import Hero from "@/components/Hero"
+import Navbar from "@/components/Navbar"
 import { Button } from "@/components/ui/button"
+import WhatWeDo from "@/components/WhatWeDo"
 
 export default function Home() {
   return (
-    <div>
-      Hi There!
+    <div className="relative min-h-screen text-white w-full bg-black">
+      <Navbar/>
+      <Hero/>
+
+      <WhatWeDo/>
+
+      <Footer/>
     </div>
   )
 }
