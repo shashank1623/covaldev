80d249fc4571afea56f4d70c13eedb91e1f097dc
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-10-06 22:34:59 +0530

    hero section spotlight animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,22 @@ const config: Config = {
   darkMode : "class",
   theme: {
   	extend: {
+		animation: {
+			spotlight: "spotlight 2s ease .75s 1 forwards",
+		  },
+		  keyframes: {
+			spotlight: {
+			  "0%": {
+				opacity: "0",
+				transform: "translate(-72%, -62%) scale(0.5)",
+			  },
+			  "100%": {
+				opacity: "1",
+				transform: "translate(-50%,-40%) scale(1)",
+			  },
+			},
+		  },
+	
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
