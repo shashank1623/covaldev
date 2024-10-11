2bb1ff3754186a4cc9c9b167b634c52e8e0620cd
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-10-11 20:31:57 +0530

    Route to test api

---
+++ app/api/user/route.ts
@@ -0,0 +1,9 @@
+
+export function GET(){
+    return Response.json({
+        username : "shashank1623",
+        email : "shashankbhardwaj2030@gmail.com",
+        firstName : "shashank",
+        lastName : "bhardwaj"
+    })
+}
