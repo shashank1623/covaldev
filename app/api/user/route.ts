d448de09932b72aedd0794fc85af38c229f9ab8b
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-10-11 20:41:57 +0530

    Fixed the response issue

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,6 +1,7 @@
 
+import { NextResponse } from "next/server"
 export function GET(){
-    return Response.json({
+    return NextResponse.json({
         username : "shashank1623",
         email : "shashankbhardwaj2030@gmail.com",
         firstName : "shashank",
