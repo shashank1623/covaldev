e9095c263dec7a907f4b00ffcbba3570592e85c6
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-10-12 22:42:58 +0530

    Dashboard route added to root layout

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { SidebarDemo } from "@/components/SidebarDemo";
+
+export default function Dashboard(){
+    return(
+        <div className="relative min-h-screen w-full text-white bg-black">
+          Dashboard Route
+        </div>
+    )
+}
