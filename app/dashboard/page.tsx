e373b6e0782a39a9685edd3cf9f2c6b5ef5bd266
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-10-13 21:41:09 +0530

    Dashboard updated with sidebarDemo

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { SidebarDemo } from "@/components/SidebarDemo";
 export default function Dashboard(){
     return(
         <div className="relative min-h-screen w-full text-white bg-black">
-          Dashboard Route
+          <SidebarDemo/>
         </div>
     )
 }
